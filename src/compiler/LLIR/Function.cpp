fc1942b86496e43dadaecffe726ef5ffcec0db28
Patrick Flynn <pflynn157@gmail.com>
2023-01-30 14:29:00 -0500

    Added LLIR back (partially working)

---
+++ src/compiler/LLIR/Function.cpp
@@ -0,0 +1,139 @@
+//
+// Copyright 2021 Patrick Flynn
+// This file is part of the Tiny Lang compiler.
+// Tiny Lang is licensed under the BSD-3 license. See the COPYING file for more information.
+//
+#include <iostream>
+
+#include "Compiler.hpp"
+#include <llir/llir.hpp>
+
+//
+// Compiles a function and its body
+//
+void Compiler::compileFunction(AstGlobalStatement *global) {
+    symtable.clear();
+    typeTable.clear();
+    structVarTable.clear();
+    structArgs.clear();
+    
+    AstFunction *astFunc = static_cast<AstFunction *>(global);
+
+    std::vector<Var> astVarArgs = astFunc->getArguments();
+    LLIR::Type *funcType = translateType(astFunc->getDataType());
+    currentFuncType = astFunc->getDataType();
+    if (currentFuncType->getType() == V_AstType::Struct)
+        funcTypeStruct = static_cast<AstStructType *>(astFunc->getDataType())->getName();
+    
+    std::vector<LLIR::Type *> args;
+    if (astVarArgs.size() > 0) {
+        for (auto var : astVarArgs) {
+            LLIR::Type *type = translateType(var.type);
+            //if (var.type == V_AstType::Struct) {
+            //    type = translateType(V_AstType::Ptr, var.type, var.typeName);
+            //}
+            args.push_back(type);
+        }
+    }
+    
+    LLIR::Function *func = LLIR::Function::Create(astFunc->getName(), LLIR::Linkage::Global, funcType);
+    func->setArgs(args);
+    currentFunc = func;
+    mod->addFunction(func);
+    
+    builder->setCurrentFunction(func);
+    builder->createBlock("entry");
+    
+    // Load and store any arguments
+    if (astVarArgs.size() > 0) {
+        for (int i = 0; i<astVarArgs.size(); i++) {
+            Var var = astVarArgs.at(i);
+            
+            // Build the alloca for the local var
+            LLIR::Type *type = args.at(i);
+            // TODO: Combine this with below
+            if (var.type->getType() == V_AstType::Struct) {
+                LLIR::Reg *alloca = builder->createAlloca(type);
+                symtable[var.name] = alloca;
+                typeTable[var.name] = var.type;
+                //structVarTable[var.name] = var.typeName;
+                structArgs.push_back(var.name);
+                
+                LLIR::Operand *param = func->getArg(i);
+                builder->createStore(type, param, alloca);
+                continue;
+            }
+            
+            LLIR::Reg *alloca = builder->createAlloca(type);
+            symtable[var.name] = alloca;
+            typeTable[var.name] = var.type;
+            
+            // Store the variable
+            LLIR::Operand *param = func->getArg(i);
+            builder->createStore(type, param, alloca);
+        }
+    }
+
+    for (auto stmt : astFunc->getBlock()->getBlock()) {
+        compileStatement(stmt);
+    }
+}
+
+//
+// Compiles an extern function declaration
+//
+void Compiler::compileExternFunction(AstGlobalStatement *global) {
+    AstExternFunction *astFunc = static_cast<AstExternFunction *>(global);
+    
+    std::vector<Var> astVarArgs = astFunc->getArguments();
+    LLIR::Type *funcType = translateType(astFunc->getDataType());
+    
+    std::vector<LLIR::Type *> args;
+    if (astVarArgs.size() > 0) {
+        for (auto var : astVarArgs) {
+            LLIR::Type *type = translateType(var.type);
+            args.push_back(type);
+        }
+    }
+    
+    LLIR::Function *func = LLIR::Function::Create(astFunc->getName(), LLIR::Linkage::Extern, funcType);
+    func->setArgs(args);
+    mod->addFunction(func);
+}
+
+//
+// Compiles a function call statement
+// This is different from an expression; this is where its a free-standing statement
+//
+void Compiler::compileFuncCallStatement(AstStatement *stmt) {
+    AstFuncCallStmt *fc = static_cast<AstFuncCallStmt *>(stmt);
+    std::vector<LLIR::Operand *> args;
+    
+    AstExprList *list = static_cast<AstExprList *>(fc->getExpression());
+    for (auto arg : list->getList()) {
+        LLIR::Operand *val = compileValue(arg);
+        args.push_back(val);
+    }
+    
+    builder->createVoidCall(fc->getName(), args);
+}
+
+//
+// Compiles a return statement
+// TODO: We may want to rethink this some
+//
+void Compiler::compileReturnStatement(AstStatement *stmt) {
+    if (stmt->hasExpression()) {
+        LLIR::Operand *op = compileValue(stmt->getExpression(), currentFuncType);
+        LLIR::Type *type = translateType(currentFuncType);
+        if (currentFuncType->getType() == V_AstType::Struct) {
+            LLIR::StructType *type = structTable[funcTypeStruct];
+            builder->createRet(type, op);
+        } else {
+            builder->createRet(type, op);
+        }
+    } else {
+        builder->createRetVoid();
+    }
+}
+
--- src/llir_main.cpp
+++ src/llir_main.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2021 Patrick Flynn
+// Copyright 2022 Patrick Flynn
 // This file is part of the Tiny Lang compiler.
 // Tiny Lang is licensed under the BSD-3 license. See the COPYING file for more information.
 //
@@ -11,13 +11,13 @@
 #include <parser/Parser.hpp>
 #include <ast/ast.hpp>
 
-#include <compiler/LLVM/Compiler.hpp>
+#include <compiler/LLIR/Compiler.hpp>
 
 bool isError = false;
 
 // TODO: I'm not sure actually if the lex testing actually works
 //
-AstTree *getAstTree(std::string input, bool testLex, bool printAst, bool emitDot) {
+AstTree *getAstTree(std::string input, bool testLex, bool printAst) {
     Parser *frontend = new Parser(input);
     AstTree *tree;
     
@@ -43,15 +43,10 @@ AstTree *getAstTree(std::string input, bool testLex, bool printAst, bool emitDot
         return nullptr;
     }
     
-    if (emitDot) {
-        tree->dot();
-        return nullptr;
-    }
-    
     return tree;
 }
 
-int compileLLVM(AstTree *tree, CFlags flags, bool printLLVM, bool emitLLVM) {
+int compileLLIR(AstTree *tree, CFlags flags, bool printLLVM, bool printLLIR2) {
     Compiler *compiler = new Compiler(tree, flags);
     compiler->compile();
         
@@ -59,18 +54,8 @@ int compileLLVM(AstTree *tree, CFlags flags, bool printLLVM, bool emitLLVM) {
         compiler->debug();
         return 0;
     }
-    
-    if (emitLLVM) {
-        std::string output = flags.name;
-        if (output == "a.out") {
-            output = "./out.ll";
-        }
-            
-        compiler->emitLLVM(output);
-        return 0;
-    }
         
-    compiler->writeAssembly();
+    compiler->writeAssembly(printLLIR2);
     compiler->assemble();
     compiler->link();
     
@@ -92,9 +77,8 @@ int main(int argc, char **argv) {
     bool emitPreproc = false;
     bool testLex = false;
     bool printAst = false;
-    bool emitDot = false;
     bool printLLVM = false;
-    bool emitLLVM = false;
+    bool printLLIR2 = false;
     
     for (int i = 1; i<argc; i++) {
         std::string arg = argv[i];
@@ -105,12 +89,10 @@ int main(int argc, char **argv) {
             testLex = true;
         } else if (arg == "--ast") {
             printAst = true;
-        } else if (arg == "--dot") {
-            emitDot = true;
-        } else if (arg == "--llvm") {
+        } else if (arg == "--llir") {
             printLLVM = true;
-        } else if (arg == "--emit-llvm") {
-            emitLLVM = true;
+        } else if (arg == "--llir2") {
+            printLLIR2 = true;
         } else if (arg == "-o") {
             flags.name = argv[i+1];
             i += 1;
@@ -127,13 +109,13 @@ int main(int argc, char **argv) {
         return 1;
     }
     
-    AstTree *tree = getAstTree(newInput, testLex, printAst, emitDot);
+    AstTree *tree = getAstTree(newInput, testLex, printAst);
     if (tree == nullptr) {
         if (isError) return 1;
         return 0;
     }
 
     // Compile
-    return compileLLVM(tree, flags, printLLVM, emitLLVM);
+    return compileLLIR(tree, flags, printLLVM, printLLIR2);
 }
 

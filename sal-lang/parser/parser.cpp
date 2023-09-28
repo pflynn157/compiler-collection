84db5ee46462d381b538693cb5b649b2f33f2d69
Patrick Flynn <pflynn157@gmail.com>
2023-09-28 11:59:36 -0400

    (SAL) Added initial block, statement, and expression parsing

--- sal-lang/parser/parser.cpp
+++ sal-lang/parser/parser.cpp
@@ -25,10 +25,59 @@ bool Parser::parse() {
 //
 bool Parser::parse_block(std::shared_ptr<AstBlock> block) {
     token t = lex->get_next();
+    bool code = true;
+    
     while (t != t_eof) {
+        switch (t) {
+            case t_nl: break;
+            
+            case t_return: code = parse_return(block); break;
+            
+            default: {
+                syntax->addError(0, "Unknown token in block");
+                lex->print(t);
+                return false;
+            }
+        }
+        
+        if (!code) return false;
         t = lex->get_next();
     }
 
     return true;
+}
+
+//
+// Parses a return statement
+//
+bool Parser::parse_return(std::shared_ptr<AstBlock> block) {
+    std::shared_ptr<AstReturnStmt> ret = std::make_shared<AstReturnStmt>();
+    ret->expression = parse_expression(block);
+    block->addStatement(ret);
+
+    return true;
+}
+
+//
+// Parses an expression
+//
+std::shared_ptr<AstExpression> Parser::parse_expression(std::shared_ptr<AstBlock> block, token stop) {
+    std::shared_ptr<ExprContext> context = std::make_shared<ExprContext>();
+    
+    token t = lex->get_next();
+    while (t != t_eof && t != stop) {
+        switch (t) {
+            case t_int_literal: {
+                int val = std::stoi(lex->value);
+                std::shared_ptr<AstI32> i = std::make_shared<AstI32>(val);
+                context->output.push(i);
+            } break;
+        }
+        
+        t = lex->get_next();
+    }
+    
+    if (context->output.empty()) return nullptr;
+    return context->output.top();
 }
 
--- sal-lang/parser/parser.hpp
+++ sal-lang/parser/parser.hpp
@@ -12,6 +12,8 @@ public:
     bool parse();
     
     bool parse_block(std::shared_ptr<AstBlock> block);
+    bool parse_return(std::shared_ptr<AstBlock> block);
+    std::shared_ptr<AstExpression> parse_expression(std::shared_ptr<AstBlock> block, token stop = t_nl);
     
     std::unique_ptr<Lex> lex;
 };

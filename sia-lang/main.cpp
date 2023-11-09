b2e95f469838cb3ab71946532ea5579753c9175a
Patrick Flynn <pflynn157@gmail.com>
2023-11-09 11:03:56 -0500

    [SIA] Added base code for assembling and linking

--- sia-lang/main.cpp
+++ sia-lang/main.cpp
@@ -16,6 +16,42 @@ void test_lex(std::string input) {
     lex->debug_token(t);
 }
 
+void assemble(CFlags cflags) {
+    std::string cmd = "as /tmp/" + cflags.name + ".asm -o /tmp/" + cflags.name + ".o";
+    system(cmd.c_str());
+}
+
+#ifdef DEV_LINK_MODE
+
+#ifndef LINK_LOCATION
+#define LINK_LOCATION = "."
+#endif
+
+void link(CFlags cflags) {
+    std::string cmd = "ld ";
+    cmd += "/usr/lib/x86_64-linux-gnu/crt1.o ";
+    cmd += "/usr/lib/x86_64-linux-gnu/crti.o ";
+    cmd += "/usr/lib/x86_64-linux-gnu/crtn.o ";
+    cmd += "/tmp/" + cflags.name + ".o -o " + cflags.name;
+    cmd += " -dynamic-linker /lib64/ld-linux-x86-64.so.2 ";
+    cmd += "-lc";
+    system(cmd.c_str());
+    //printf("LINK: %s\n", cmd.c_str());
+}
+
+#else
+
+void link(CFlags cflags) {
+    /*std::string cmd = "ld ";
+    cmd += "/usr/local/lib/tinylang/ti_start.o ";
+    cmd += "/tmp/" + cflags.name + ".o -o " + cflags.name;
+    cmd += " -dynamic-linker /lib64/ld-linux-x86-64.so.2 ";
+    cmd += "-ltinylang -lc";
+    system(cmd.c_str());*/
+}
+
+#endif
+
 int main(int argc, char **argv) {
     std::string input = "../first.sia";
     test_lex(input);
@@ -37,6 +73,9 @@ int main(int argc, char **argv) {
     compiler->writeAssembly();
     
     compiler->debug();
+    
+    assemble(flags);
+    link(flags);
     
     return 0;
 }

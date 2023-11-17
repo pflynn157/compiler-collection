4748d3b6015fab98ac12e3f39d473bacfd7bb3ff
Patrick Flynn <pflynn157@gmail.com>
2023-11-17 09:49:53 -0700

    [Compiler] Added lexical generator for keywords and symbols

--- lex/lex.cpp
+++ lex/lex.cpp
@@ -90,38 +90,7 @@ token Lex::get_next() {
             if (buffer == "") continue;
             
             token t = t_none;
-            if (buffer == "extern") t = t_extern;
-            else if (buffer == "func") t = t_func;
-            else if (buffer == "struct") t = t_struct;
-            else if (buffer == "end") t = t_end;
-            else if (buffer == "return") t = t_return;
-            else if (buffer == "var") t = t_var;
-            else if (buffer == "const") t = t_const;
-            else if (buffer == "bool") t = t_bool;
-            else if (buffer == "char") t = t_char;
-            else if (buffer == "string") t = t_string;
-            else if (buffer == "i8") t = t_i8;
-            else if (buffer == "u8") t = t_u8;
-            else if (buffer == "i16") t = t_i16;
-            else if (buffer == "u16") t = t_u16;
-            else if (buffer == "i32") t = t_i32;
-            else if (buffer == "u32") t = t_u32;
-            else if (buffer == "i64") t = t_i64;
-            else if (buffer == "u64") t = t_u64;
-            else if (buffer == "if") t = t_if;
-            else if (buffer == "elif") t = t_elif;
-            else if (buffer == "else") t = t_else;
-            else if (buffer == "while") t = t_while;
-            else if (buffer == "is") t = t_is;
-            else if (buffer == "then") t = t_then;
-            else if (buffer == "do") t = t_do;
-            else if (buffer == "break") t = t_break;
-            else if (buffer == "continue") t = t_continue;
-            else if (buffer == "import") t = t_import;
-            else if (buffer == "true") t = t_true;
-            else if (buffer == "false") t = t_false;
-            else if (buffer == "and") t = t_lgand;
-            else if (buffer == "or") t = t_lgor;
+            ///LEX_KEYWORD_CHECK
             else if (is_integer()) {
                 t = t_int_literal;
                 value = buffer;
@@ -147,26 +116,7 @@ token Lex::get_next() {
 
 bool Lex::is_symbol(char c) {
     switch (c) {
-        case '.': return true;
-        case ';': return true;
-        case ',': return true;
-        case '(': return true;
-        case ')': return true;
-        case '[': return true;
-        case ']': return true;
-        case '+': return true;
-        case '-': return true;
-        case '*': return true;
-        case '/': return true;
-        case '%': return true;
-        case '&': return true;
-        case '|': return true;
-        case '^': return true;
-        case ':': return true;
-        case '>': return true;
-        case '<': return true;
-        case '=': return true;
-        case '!': return true;
+        ///LEX_SYMBOL_CHECK
         
         default: return false;
     }
@@ -175,70 +125,7 @@ bool Lex::is_symbol(char c) {
 
 token Lex::get_symbol(char c) {
     switch (c) {
-        case '.': return t_dot;
-        case ';': return t_semicolon;
-        case ',': return t_comma;
-        case '(': return t_lparen;
-        case ')': return t_rparen;
-        case '[': return t_lbracket;
-        case ']': return t_rbracket;
-        case '+': return t_plus;
-        case '-': {
-            char c2 = reader.get();
-            if (c2 == '>') {
-                //rawBuffer += c2;
-                return t_arrow;
-            } else {
-                reader.unget();
-                return t_minus;
-            }
-        } break;
-        case '*': return t_mul;
-        case '/': return t_div;
-        case '%': return t_mod;
-        case '&': return t_and;
-        case '|': return t_or;
-        case '^': return t_xor;
-        case ':': {
-            char c2 = reader.get();
-            if (c2 == '=') {
-                //rawBuffer += c2;
-                return t_assign;
-            } else {
-                reader.unget();
-                return t_colon;
-            }
-        } break;
-        case '>': {
-            char c2 = reader.get();
-            if (c2 == '=') {
-                //rawBuffer += c2;
-                return t_gte;
-            } else {
-                reader.unget();
-                return t_gt;
-            }
-        } break;
-        case '<': {
-            char c2 = reader.get();
-            if (c2 == '=') {
-                //rawBuffer += c2;
-                return t_lte;
-            } else {
-                reader.unget();
-                return t_lt;
-            }
-        } break;
-        case '=': return t_eq;
-        case '!': {
-            char c2 = reader.get();
-            if (c2 == '=') {
-                //rawBuffer += c2;
-                return t_neq;
-            } else {
-                reader.unget();
-            }
-        } break;
+        ///LEX_SYMBOL_RETURN
         default: return t_none;
     }
     return t_none;
@@ -269,62 +156,9 @@ void Lex::debug_token(token t) {
         case t_none: std::cout << "???" << std::endl; break;
         case t_eof: std::cout << "EOF" << std::endl; break;
         
-        case t_extern: std::cout << "extern" << std::endl; break;
-        case t_func: std::cout << "func" << std::endl; break;
-        case t_struct: std::cout << "struct" << std::endl; break;
-        case t_end: std::cout << "end" << std::endl; break;
-        case t_return: std::cout << "return" << std::endl; break;
-        case t_var: std::cout << "var" << std::endl; break;
-        case t_const: std::cout << "const" << std::endl; break;
-        case t_bool: std::cout << "bool" << std::endl; break;
-        case t_char: std::cout << "char" << std::endl; break;
-        case t_string: std::cout << "string" << std::endl; break;
-        case t_i8: std::cout << "i8" << std::endl; break;
-        case t_u8: std::cout << "u8" << std::endl; break;
-        case t_i16: std::cout << "i16" << std::endl; break;
-        case t_u16: std::cout << "u16" << std::endl; break;
-        case t_i32: std::cout << "i32" << std::endl; break;
-        case t_u32: std::cout << "u32" << std::endl; break;
-        case t_i64: std::cout << "i64" << std::endl; break;
-        case t_u64: std::cout << "u64" << std::endl; break;
-        case t_if: std::cout << "if" << std::endl; break;
-        case t_elif: std::cout << "elif" << std::endl; break;
-        case t_else: std::cout << "else" << std::endl; break;
-        case t_while: std::cout << "while" << std::endl; break;
-        case t_is: std::cout << "is" << std::endl; break;
-        case t_then: std::cout << "then" << std::endl; break;
-        case t_do: std::cout << "do" << std::endl; break;
-        case t_break: std::cout << "break" << std::endl; break;
-        case t_continue: std::cout << "continue" << std::endl; break;
-        case t_import: std::cout << "import" << std::endl; break;
-        case t_true: std::cout << "true" << std::endl; break;
-        case t_false: std::cout << "false" << std::endl; break;
-        case t_lgand: std::cout << "and" << std::endl; break;
-        case t_lgor: std::cout << "or" << std::endl; break;
-        case t_dot: std::cout << "." << std::endl; break;
-        case t_semicolon: std::cout << ";" << std::endl; break;
-        case t_comma: std::cout << "," << std::endl; break;
-        case t_lparen: std::cout << "(" << std::endl; break;
-        case t_rparen: std::cout << ")" << std::endl; break;
-        case t_lbracket: std::cout << "[" << std::endl; break;
-        case t_rbracket: std::cout << "]" << std::endl; break;
-        case t_plus: std::cout << "+" << std::endl; break;
-        case t_minus: std::cout << "-" << std::endl; break;
-        case t_arrow: std::cout << "->" << std::endl; break;
-        case t_mul: std::cout << "*" << std::endl; break;
-        case t_div: std::cout << "/" << std::endl; break;
-        case t_mod: std::cout << "%" << std::endl; break;
-        case t_and: std::cout << "&" << std::endl; break;
-        case t_or: std::cout << "|" << std::endl; break;
-        case t_xor: std::cout << "^" << std::endl; break;
-        case t_colon: std::cout << ":" << std::endl; break;
-        case t_assign: std::cout << ":=" << std::endl; break;
-        case t_gt: std::cout << ">" << std::endl; break;
-        case t_gte: std::cout << ">=" << std::endl; break;
-        case t_lt: std::cout << "<" << std::endl; break;
-        case t_lte: std::cout << "<=" << std::endl; break;
-        case t_eq: std::cout << "=" << std::endl; break;
-        case t_neq: std::cout << "!=" << std::endl; break;
+        ///LEX_KEYWORD_DEBUG
+        
+        ///LEX_SYMBOL_DEBUG
         
         case t_id: std::cout << "ID(" << value << ")" << std::endl; break;
         case t_string_literal: std::cout << "STR(" << value << ")" << std::endl; break;
--- lex/lex.py
+++ lex/lex.py
@@ -6,9 +6,119 @@ from shutil import copyfile
 sys.path.append(os.getcwd())
 
 import config
+keywords = config.keywords
+symbols = dict()
+
+for item in config.symbols:
+	name = item[0]
+	value = item[1]
+	key = value[0]		# This is the first character of the symbol sequence
+	if key in symbols:
+		symbols[key].append((value, name))
+	else:
+		symbols[key] = [(value, name)]
 
 base_path = sys.argv[1]
 
-copyfile(base_path + "/lex.cpp", "./lex.cpp")
-copyfile(base_path + "/lex.hpp", "./lex.hpp")
+#copyfile(base_path + "/lex.cpp", "./lex.cpp")
+#copyfile(base_path + "/lex.hpp", "./lex.hpp")
+
+##
+## Generate the header file
+##
+writer = open("./lex.hpp", "w")
+
+with open(base_path + "/lex.hpp", "r") as reader:
+    for line in reader:
+        ln = line.strip()
+        
+        if ln == "///LEX_KEYWORDS":
+            for keyword in keywords:
+                writer.write("\t" + keyword[0] + ",\n")
+        
+        elif ln == "///LEX_SYMBOLS":
+            for sym in config.symbols:
+                writer.write("\t" + sym[0] + ",\n")
+        
+        else:
+            writer.write(line)
+
+writer.close()
+
+##
+## Generate the source file
+##
+writer = open("./lex.cpp", "w")
+
+with open(base_path + "/lex.cpp", "r") as reader:
+    for line in reader:
+        ln = line.strip()
+        
+        # Keyword if-else block
+        if ln == "///LEX_KEYWORD_CHECK":
+            first = True
+            for keyword in keywords:
+                writer.write("\t\t\t")
+                if first:
+                    writer.write("if ")
+                    first = False
+                else:
+                    writer.write("else if ")
+                writer.write("(buffer == \"" + keyword[1] + "\") t = " + keyword[0] + ";\n")
+        
+        # Keyword debug section
+        elif ln == "///LEX_KEYWORD_DEBUG":
+            for keyword in keywords:
+                writer.write("\t\tcase " + keyword[0] + ": std::cout << \"" + keyword[1] + "\" << std::endl; break;\n")
+        
+        # Symbol debug section
+        elif ln == "///LEX_SYMBOL_DEBUG":
+            for sym in config.symbols:
+                writer.write("\t\tcase " + sym[0] + ": std::cout << \"" + sym[1] + "\" << std::endl; break;\n")
+        
+        # Symbol checking
+        elif ln == "///LEX_SYMBOL_CHECK":
+            for value, name in symbols.items():
+                writer.write("\t\tcase \'" + value[0] + "\': return true;\n")
+        
+        # Return the proper symbol
+        elif ln == "///LEX_SYMBOL_RETURN":
+            for value, name_list in symbols.items():
+                if len(name_list) == 1 and len(name_list[0][0]) == 1:
+                    writer.write("\t\tcase \'" + value + "\': return " + name_list[0][1] + ";\n")
+                else:
+                    writer.write("\t\tcase \'" + value + "\': {\n")
+                    writer.write("\t\t\tchar c2 = reader.get();\n")
+
+                    found_first = False
+                    default_name = None
+
+                    for item in name_list:
+                        if len(item[0]) == 1:
+                            default_name = item[1]
+                            continue
+
+                        symbol = item[0]
+                        name = item[1]
+                        
+                        if found_first:
+                            writer.write("\t\t\t} else ")
+                        found_first = True
+                        writer.write("\t\t\tif (c2 == \'" + symbol[1] + "\') {\n")
+                        writer.write("\t\t\t\treturn " + name + ";\n")
+                        
+                    # Final else statement
+                    writer.write("\t\t\t} else {\n")
+                    writer.write("\t\t\t\treader.unget();\n")
+                    if default_name != None:
+                        writer.write("\t\t\t\treturn " + default_name + ";\n")
+                    writer.write("\t\t\t}\n")
+
+                    # Closing brace of break statement
+                    writer.write("\t\t} break;\n")
+                
+        else:
+            writer.write(line)
+
+writer.close()
 

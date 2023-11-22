060e76b1861f2d312d4c819e2aa3a841ebfab29e
Patrick Flynn <pflynn157@gmail.com>
2023-11-22 08:28:12 -0500

    [Orka] Enforced variable-only syntax; Updated all tests

--- orka-lang/parser/Variable.cpp
+++ orka-lang/parser/Variable.cpp
@@ -48,106 +48,29 @@ bool Parser::buildVariableDec(std::shared_ptr<AstBlock> block) {
     std::shared_ptr<AstDataType> dataType = buildDataType(false);
     tk = lex->get_next();
     
-    // We have an array
-    if (tk == t_lbracket) {
-        std::shared_ptr<AstDataType> baseType = dataType;
-        std::string dataType_name = getArrayType(dataType);
-        dataType = AstBuilder::buildStructType(dataType_name);
-        std::shared_ptr<AstStructDec> dec = std::make_shared<AstStructDec>("", dataType_name);
-        
-        std::shared_ptr<AstExpression> size_arg = buildExpression(block, AstBuilder::buildInt32Type(), t_rbracket);
-        if (!size_arg) {
-            syntax->addError(lex->line_number, "Invalid size argument in array declaration");
-            return false;
-        }
-        //empty->expression = arg; 
-        
-        tk = lex->get_next();
-        if (tk != t_semicolon) {
-            syntax->addError(lex->line_number, "Error: Expected \';\'.");
-            return false;
-        }
-        
-        for (std::string name : toDeclare) {
-            std::shared_ptr<AstStructDec> dec = std::make_shared<AstStructDec>(name, dataType_name);
-            dec->no_init = true;
-            block->addStatement(dec);
-            
-            //
-            // Malloc
-            //
-            std::shared_ptr<AstExprList> list = std::make_shared<AstExprList>();
-            
-            // Get the size
-            //std::shared_ptr<AstI32> size = std::make_shared<AstI32>(4);
-            std::shared_ptr<AstInt> size;
-            if (baseType->type == V_AstType::Int32) size = std::make_shared<AstInt>(4);
-            else if (baseType->type == V_AstType::Int64) size = std::make_shared<AstInt>(8);
-            else if (baseType->type == V_AstType::String) size = std::make_shared<AstInt>(8);
-            else size = std::make_shared<AstInt>(1);
-            
-            std::shared_ptr<AstMulOp> mul_op = std::make_shared<AstMulOp>();
-            mul_op->lval = size;
-            mul_op->rval = size_arg;
-            list->add_expression(mul_op);
-            
-            // Create the malloc call
-            std::shared_ptr<AstFuncCallExpr> call_malloc = std::make_shared<AstFuncCallExpr>("gc_alloc");
-            call_malloc->args = list;
-            
-            std::shared_ptr<AstStructAccess> lval_alloc = std::make_shared<AstStructAccess>(name, "ptr");
-            std::shared_ptr<AstAssignOp> op1 = std::make_shared<AstAssignOp>();
-            op1->lval = lval_alloc;
-            op1->rval = call_malloc;
-            
-            std::shared_ptr<AstExprStatement> va_ptr = std::make_shared<AstExprStatement>();
-            va_ptr->setDataType(dataType);
-            va_ptr->expression = op1;
-            block->addStatement(va_ptr);
-            
-            //
-            // Set the size
-            //
-            // expression
-            std::shared_ptr<AstStructAccess> lval_size = std::make_shared<AstStructAccess>(name, "size");
-            std::shared_ptr<AstAssignOp> op = std::make_shared<AstAssignOp>();
-            op->lval = lval_size;
-            op->rval = size_arg;
-            
-            // The statement
-            std::shared_ptr<AstExprStatement> va_size = std::make_shared<AstExprStatement>();
-            va_size->setDataType(dataType);
-            va_size->expression = op;
-            block->addStatement(va_size);
-            
-            block->addSymbol(name, dataType);
-        }
-    
     // We're at the end of the declaration
-    } else if (tk == t_semicolon) {
+    if (tk == t_semicolon) {
         syntax->addError(lex->line_number, "Expected init expression.");
         return false;
-        
-    // Otherwise, we have a regular variable
-    } else {
-        std::shared_ptr<AstExpression> arg = buildExpression(block, dataType, t_semicolon);
-        if (!arg) return false;
+    }
     
-        for (std::string name : toDeclare) {
-            std::shared_ptr<AstVarDec> vd = std::make_shared<AstVarDec>(name, dataType);
-            block->addStatement(vd);
-            
-            std::shared_ptr<AstID> id = std::make_shared<AstID>(name);
-            std::shared_ptr<AstAssignOp> assign = std::make_shared<AstAssignOp>(id, arg);
-            
-            std::shared_ptr<AstExprStatement> va = std::make_shared<AstExprStatement>();
-            va->setDataType(dataType);
-            va->expression = assign;
-            block->addStatement(va);
-            
-            // Add the variable to the blocks symbol table
-            block->addSymbol(name, dataType);
-        }
+    std::shared_ptr<AstExpression> arg = buildExpression(block, dataType, t_semicolon);
+    if (!arg) return false;
+
+    for (std::string name : toDeclare) {
+        std::shared_ptr<AstVarDec> vd = std::make_shared<AstVarDec>(name, dataType);
+        block->addStatement(vd);
+        
+        std::shared_ptr<AstID> id = std::make_shared<AstID>(name);
+        std::shared_ptr<AstAssignOp> assign = std::make_shared<AstAssignOp>(id, arg);
+        
+        std::shared_ptr<AstExprStatement> va = std::make_shared<AstExprStatement>();
+        va->setDataType(dataType);
+        va->expression = assign;
+        block->addStatement(va);
+        
+        // Add the variable to the blocks symbol table
+        block->addSymbol(name, dataType);
     }
     
     return true;
